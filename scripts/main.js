a58b0b8ac61fb93d35f038b8b958628ae20828ac
juanez1999 <juanez1999@gmail.com>
2020-10-12 20:27:38 -0500

    Add firestore

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,18 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCmSHBNjZ1NJWKvM-TzvF5BI5IN26ZXJJg",
+  authDomain: "web-js-project.firebaseapp.com",
+  databaseURL: "https://web-js-project.firebaseio.com",
+  projectId: "web-js-project",
+  storageBucket: "web-js-project.appspot.com",
+  messagingSenderId: "998843852384",
+  appId: "1:998843852384:web:1c882d575c8a2c84f68505",
+  measurementId: "G-DKERXLDK2M"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore();
+
 const products = [
   {
     title: 'Celular Motorola Moto One Macro',
@@ -36,16 +51,34 @@ function renderProducts (list) {
     <div class="product__info">
       <h3 class="product__title">${elem.title}</h3>
       <p class="product__price">$ ${elem.price}</p>
+      <button class="product__delete">Eliminar</button>
+      <button class="product__edit">Editar</button>
     </div>
     `;
   
     productsList.appendChild(newProduct);
   });
+  deleteProducts();
+  updateProducts();
 }
 
+var objects = [];
+function getProducts(){
+  db.collection("products").get().then((querySnapshot) => {
+    objects.splice(0,objects.length);
+    querySnapshot.forEach((doc) => {
+        const obj = doc.data();
+        obj.id = doc.id;
+        objects.push(obj);
+        console.log(`${doc.id} => ${doc.data()}`);
+    });
+    renderProducts(objects);
+  });
+}
 
+getProducts();
 // render inicial con todos los productos
-renderProducts(products);
+//renderProducts(products);
 
 
 const filterBtn = document.querySelector('.filterbtn');
@@ -68,7 +101,7 @@ filterBtn.addEventListener('click', function () {
 
 
 
-
+//Aqui es donde agregamos un producto
 const form = document.querySelector('.form');
 form.addEventListener('submit', function (event) {
   event.preventDefault();
@@ -79,7 +112,47 @@ form.addEventListener('submit', function (event) {
     price: form.price.value
   };
 
-  products.push(newProduct);
+  db.collection("products").add(newProduct)
+  .then(function(docRef) {
+      console.log("Document written with ID: ", docRef.id);
+  })
+  .catch(function(error) {
+      console.error("Error adding document: ", error);
+  });
 
-  renderProducts(products);
+  //products.push(newProduct);
+
+  getProducts();
+  //renderProducts(products);
 });
+
+function deleteProducts(){
+  var deleteBtns = document.querySelectorAll('.product__delete');
+  deleteBtns.forEach(function (btn,index){
+    btn.addEventListener('click',function(){
+
+      db.collection("products").doc(objects[index].id).delete().then(function() {
+        getProducts();
+        console.log("Document successfully deleted!");
+      }).catch(function(error) {
+          console.error("Error removing document: ", error);
+      });
+
+    });
+  });
+}
+
+function updateProducts(){
+  /*db.collection("cities").doc("LA").set({
+    name: "Los Angeles",
+    state: "CA",
+    country: "USA"
+  })
+  .then(function() {
+      console.log("Document successfully written!");
+  })
+  .catch(function(error) {
+      console.error("Error writing document: ", error);
+  });*/
+}
+
